6582c33ff9727a0ab6fe63c549fd4573be80500b
Bloob <acceleration9992@gmail.com>
2026-06-29 21:55:16 -0700

    Change manifest for Patch Alpha 1.2

--- AlphaScan.py
+++ AlphaScan.py
@@ -1,25 +1,56 @@
-"""This file acts as the main module for this script."""
-
-import adsk.core
-import adsk.fusion
-import traceback, time, os, re, json
+import adsk.core, adsk.fusion
+import traceback, os
+import time, re, json
+import urllib.request as requests
 
 app = adsk.core.Application.get()
-ui  = app.userInterface
+ui = app.userInterface
 
+cmdDef = None
+handlers = []
 
-def run(_context: str):
-  """This function is called by Fusion when the script is run."""
+class MyCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
+  def notify(self, args): # type: ignore
+    runAlphaScan()
 
+def runAlphaScan():
   try:
     product = app.activeProduct
     design = adsk.fusion.Design.cast(product)
 
     if not design:
-      ui.messageBox('No active Fusion design found.', 'Error', adsk.core.MessageBoxButtonTypes.OKButtonType, adsk.core.MessageBoxIconTypes.WarningIconType) # type: ignore
+      ui.messageBox('No active Fusion design found.', 'Warning', adsk.core.MessageBoxButtonTypes.OKButtonType, adsk.core.MessageBoxIconTypes.WarningIconType) # type: ignore
       return
     
     startTime = time.time()
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+
+    def decodeVersionName(name: str):
+      data = name.strip().split(' ')[:2]
+      release, patch = data
+      releaseNo, patch = patch.split('.')
+      return 'alpha'.split(' ').index(release.lower()) * 1000 + int(releaseNo) * 100 + int(patch) # type: ignore
+
+    with requests.urlopen("https://raw.githubusercontent.com/Coder5092/AlphaScan/refs/heads/main/AlphaScan.manifest") as response:
+      if response is None:
+        ui.messageBox('Failed to fetch version information from the internet.\nPress OK to continue scanning', 'Warning', adsk.core.MessageBoxButtonTypes.OKButtonType, adsk.core.MessageBoxIconTypes.WarningIconType) # type: ignore
+      else:
+        networkManifest = json.load(response)
+        networkVersion = networkManifest['version']
+        with open(os.path.join(this_dir, "AlphaScan.manifest"), 'r') as file:
+          manifest = json.load(file)
+          version = manifest['version']
+
+          networkInt = decodeVersionName(networkVersion)
+          localInt = decodeVersionName(version)
+
+          if networkInt > localInt:
+            ui.messageBox(f'The latest online version of AlphaScan ({networkVersion}) is newer than the current installed version ({version}). Please upgrade AlphaScan!', 'Error', adsk.core.MessageBoxButtonTypes.OKButtonType, adsk.core.MessageBoxIconTypes.CriticalIconType) # type: ignore
+            return
+          elif networkInt < localInt:
+            # How did you even manage this?
+            ui.messageBox(f'The latest online version of AlphaScan ({networkVersion}) is older than the current installed version ({version}). Please downgrade AlphaScan!', 'Error', adsk.core.MessageBoxButtonTypes.OKButtonType, adsk.core.MessageBoxIconTypes.CriticalIconType) # type: ignore
+            return
     
     components = design.allComponents
     assemblies = design.rootComponent.allJoints
@@ -46,7 +77,7 @@ $andymarkComponents
       if not sketch.isFullyConstrained:
         message += sketch.name + ' is not fully constrained\n'
     
-    def jointless(occurrence: adsk.fusion.Occurrence):
+    def jointless(occurrence: adsk.fusion.Occurrence, vendor = False):
       count = 0
       try:
         count += occurrence.joints.count
@@ -62,18 +93,19 @@ $andymarkComponents
         pass
 
       if count == 0:
-          if not occurrence.isGrounded:
-            if not occurrence.assemblyContext:
-              for j in range(occurrence.childOccurrences.count):
-                if not jointless(occurrence.childOccurrences.item(j)):
-                  break
-              else:
-                return True
+        if not occurrence.isGrounded:
+          if not occurrence.assemblyContext:
+            for j in range(occurrence.childOccurrences.count * vendor):
+              if not jointless(occurrence.childOccurrences.item(j), vendor):
+                break
+            else:
+              return True
       return False
 
-    this_dir = os.path.dirname(os.path.realpath(__file__))
     with open(os.path.join(this_dir, "config/config.json"), 'r') as file:
       config = json.load(file)
+
+    # allOccurrences: list[adsk.fusion.Occurrence] = []
 
     hasHoleIssue = False
     linkedComponents = 0
@@ -91,9 +123,12 @@ $andymarkComponents
         continue
 
       occs = design.rootComponent.occurrencesByComponent(componentType)
+      # for i in range(occs.count): allOccurrences.append(occs.item(i))
+
       isGobilda = re.match(r"\d{4}-\d{4}-\d{4}", componentType.name) is not None
       isRev = re.match(r"REV-\d{2}-\d{4}", componentType.name) is not None
       isAndyMark = re.match(r"am-\d{4}(_[a-z]+)?", componentType.name) is not None
+      isVendor = isGobilda or isRev or isAndyMark
       if isGobilda: vendorComponents[0] += occs.count
       if isRev: vendorComponents[1] += occs.count
       if isAndyMark: vendorComponents[2] += occs.count
@@ -102,10 +137,10 @@ $andymarkComponents
         occurrence = occs.item(i)
         occurrenceCount += 1
 
-        if jointless(occurrence):
+        if jointless(occurrence, isVendor):
           message += occurrence.name + ' is not jointed\n'
 
-        if occurrence.isReferencedComponent and (isGobilda or isRev or isAndyMark):
+        if occurrence.isReferencedComponent and isVendor:
           linkedComponents += 1
         else:
           bodies = list(filter(lambda body: body.isSolid, [occurrence.bRepBodies.item(i) for i in range(occurrence.bRepBodies.count)]))
@@ -137,12 +172,87 @@ $andymarkComponents
               if amt > 0:
                 message += body.name + ' in component ' + occurrence.name + f' has {amt} {name} hole{"s" if amt != 1 else ""} that have too low diametrical clearance (minimum {config["holes"][i]["minClearance"] * 10} mm)\n'
 
-            hasHoleIssue = sum(totalSmallHoles.values())
-    
+            hasHoleIssue |= sum(totalSmallHoles.values()) > 0
+
+    # grounded = tuple(filter(lambda o: o.isGrounded and not o.assemblyContext, allOccurrences))
+    # reachable: set[str] = set()
+    # trace = ""
+    # trace = ""
+
+    # nodes = {o.fullPathName: o for o in allOccurrences}
+    # adj = defaultdict(set)
+
+    # def link(a, b):
+    #   if a is None or b is None:
+    #     return
+    #   if a.fullPathName == b.fullPathName:
+    #     return
+    #   adj[a.fullPathName].add(b.fullPathName)
+    #   adj[b.fullPathName].add(a.fullPathName)
+
+    # for node in allOccurrences:
+    #   try:
+    #     for i in range(node.joints.count):
+    #       j = node.joints.item(i)
+    #       link(j.occurrenceOne, j.occurrenceTwo)
+    #   except Exception as e:
+    #       trace += f"joints: {e}\n"
+
+    #   try:
+    #     for i in range(node.asBuiltJoints.count):
+    #       j = node.asBuiltJoints.item(i)
+    #       link(j.occurrenceOne, j.occurrenceTwo)
+    #   except Exception as e:
+    #     trace += f"asBuiltJoints: {e}\n"
+
+    #   try:
+    #     for i in range(node.rigidGroups.count):
+    #       g = node.rigidGroups.item(i)
+
+    #       members = [g.occurrences.item(j) for j in range(g.occurrences.count)]
+
+    #       for i2 in range(len(members)):
+    #         for j2 in range(i2 + 1, len(members)):
+    #           link(members[i2], members[j2])
+    #   except Exception as e:
+    #     trace += f"rigidGroups: {e}\n"
+
+    #   # parent-child rigidity assumption
+    #   try:
+    #     if node.assemblyContext:
+    #       link(node, node.assemblyContext)
+    #   except Exception as e:
+    #     trace += f"assemblyContext: {e}\n"
+
+    # reachable = set()
+    # q = deque()
+
+    # for o in grounded:
+    #   key = o.fullPathName
+    #   reachable.add(key)
+    #   q.append(key)
+
+    # while q:
+    #   cur = q.popleft()
+
+    #   for nxt in adj[cur]:
+    #     if nxt not in reachable:
+    #       reachable.add(nxt)
+    #       q.append(nxt)
+
+    # with open(os.path.join(this_dir, 'trace.txt'), 'w') as file:
+    #   file.write(trace)
+
+    # for node in filter(lambda o: o.fullPathName not in reachable, allOccurrences):
+    #   message += node.name + ' is floating\n'
+
+    # out-of-dates
     refs = app.activeDocument.documentReferences
     if refs:
-      if any([refs.item(i).isOutOfDate for i in range(refs.count)]):
-        message += 'Some references are out of date\n'
+      for i in range(refs.count):
+        reference = refs.item(i)
+        if reference.isOutOfDate:
+          message += reference.referencedDocument.name + ' is out of date\n'
 
     message = message.strip()
     message += '\n\n========== OTHER ==========\n'
@@ -186,6 +296,42 @@ $andymarkComponents
 
     palette.isVisible = True
     palette.dockingState = adsk.core.PaletteDockingStates.PaletteDockStateRight
-    # ui.messageBox(message, 'AlphaScan Results', adsk.core.MessageBoxButtonTypes.OKButtonType, adsk.core.MessageBoxIconTypes.InformationIconType) # type: ignore
   except:
-    ui.messageBox(f'Failed:\n{traceback.format_exc()}') # type: ignore
+    ui.messageBox(f'Failed:\n{traceback.format_exc()}', 'Error', adsk.core.MessageBoxButtonTypes.OKButtonType, adsk.core.MessageBoxIconTypes.CriticalIconType) # type: ignore
+
+
+def run(context):
+  global cmdDef
+
+  workspaces = ui.workspaces.itemById('FusionSolidEnvironment')
+  panel = workspaces.toolbarPanels.itemById('InspectPanel')
+
+  cmdDef = ui.commandDefinitions.addButtonDefinition(
+    'AlphaScanButton',
+    'Run AlphaScan',
+    'Scans the current CAD file.',
+    './assets'
+  )
+
+  panel.controls.addCommand(cmdDef) # type: ignore
+
+  onCreated = MyCommandCreatedHandler()
+  cmdDef.commandCreated.add(onCreated)
+  handlers.append(onCreated)
+
+  # cmdDef.execute() # type: ignore
+
+def stop(context):
+  global cmdDef
+  try:
+    workspace = ui.workspaces.itemById('FusionSolidEnvironment')
+    panel = workspace.toolbarPanels.itemById('InspectPanel')
+
+    ctrl = panel.controls.itemById('AlphaScanButton')
+    if ctrl:
+      ctrl.deleteMe()
+
+    if cmdDef:
+      cmdDef.deleteMe()
+  except:
+    ui.messageBox('Stop failed:\n{}'.format(traceback.format_exc())) # type: ignore
--- README.md
+++ README.md
@@ -14,14 +14,22 @@ AlphaScan is a Fusion 360 script that scans your CAD for mistakes.
 
 ## Known Issues
 
-- Two or more jointed components that can still be dragged are not detected (will fix)
-- Cannot detect warnings/errors in timeline (works as intended)
+- Two or more jointed components that are still floating as an "island" are not detected (created Alpha 1.0, will not fix as of Alpha 1.2)
+- Cannot detect warnings/errors in timeline (created Alpha 1.0, works as intended as of Alpha 1.0)
 
 ## Installation Instructions
 
 First, download all code in this repository as a ZIP file. After extraction, open any Fusion 360 design file, select the Utilities tab, then click "Scripts and Add-Ins." Ensure the name of the folder is "AlphaScan," then click the plus sign and select the script folder. After selecting "Open" or "Select Folder" in the popup file browser, AlphaScan is ready to run in the plugin dialog.
 
 ## Release History
+
+### Patch Alpha 1.2 (6/29/2026)
+
+Converted to Add-In with custom button. \
+Added forced updates. \
+Clarified which references are out of date. \
+Fixed bearing hole name in config. \
+Fixed 'No hole issues' showing with hole issues.
 
 ### Hotfix Alpha 1.1 (6/29/2026)
 
